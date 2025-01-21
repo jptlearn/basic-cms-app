76e56fde1b31f6ab2a4e6460d110c44d11f5b1d3
jptlearn <jptcode500@gmail.com>
2025-01-21 14:37:22 +0545

    add logout handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/index.js");
+
+dotenv.config();
 
 const getRegistrationPage = (req, res) => {
   console.log("rendering registration")
@@ -25,7 +29,7 @@ const registerUser = async (req, res) => {
       confirmPassword,
     })
     console.log("new user registered");
-    res.status(200).json({ message: "User registered successfully" });
+    res.redirect("/auth/user/login")
   } catch (error) {
     res.status(500).json({ message: "Error registering user!!" });
   }
@@ -46,24 +50,21 @@ const loginUser = async (req, res) => {
       return res.json({ message: "Invalid email address or password!" });
     }
     const match = await bcrypt.compare(password, user.password)
-    // if (match) {
-    //   res.json({ message: "User logged in successfully" });
-    // }
     if (match) {
-      res.status(200).json({
-        message: "User logged in successfully",
-        user: {
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          // Do not send the password
-        }
-      });
-    }
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      })
+      res.cookie("auth", token);
 
+      res.redirect("/welcomePage")
+    } else {
+      res.json({ message: "Invalid login data." })
+    }
   } catch (error) {
     res.status(500).json({ message: "Error logging user." });
   }
 }
+
+
 
 module.exports = { getRegistrationPage, getLoginPage, registerUser, loginUser };
