c08c52faaacf83f263bc46f9d50b93686230884c
jptlearn <jptcode500@gmail.com>
2025-01-21 11:18:31 +0545

    adjust view config

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const path = require("path");
+const userRoutes = require("./routes/userRoutes.js");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
 
 const initializeDatabase = require('./database/connection.js');
 
@@ -8,5 +16,13 @@ initializeDatabase().then(() => {
 }).catch((error) => {
   console.error("Error initializing the database!");
 })
+
+app.get("/", (req, res) => {
+  console.log("home page router accessed")
+  // console.log(userRoutes);
+  res.render("homePage")
+})
+
+app.use("/auth/user", userRoutes);
 
 module.exports = app;
