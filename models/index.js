57625faaaac5658fdd8cc5d919a4c12a4729b5c0
jptlearn <jptcode500@gmail.com>
2025-01-22 09:40:19 +0545

    update and clean indexjs

--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
-const User = require("./userModel.js");
-const sequelize = require("../database/dbConfig.js");
+const { Sequelize } = require("sequelize")
+const config = require("../config/database.js")
+const defineUser = require("./userModel.js")
 
-const db = {
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+})
+
+const User = defineUser(sequelize)
+
+const models = {
   User,
-  sequelize
 }
 
-module.exports = db;
+module.exports = {
+  sequelize,
+  ...models,
+}
+
