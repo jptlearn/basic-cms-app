380a4e2c36107b7cf1bc9362cc18a653f844f2bf
jptlearn <jptcode500@gmail.com>
2025-01-22 11:38:15 +0545

    add logout logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const { getRegistrationPage, getLoginPage, registerUser, loginUser } = require("../controllers/userController.js");
+const { getRegistrationPage, getLoginPage, registerUser, loginUser, logout } = require("../controllers/userController.js");
 
 const router = express.Router();
 console.log(router);
 
-router.route("/register").get((req, res) => {
-  console.log("registration route accessed");
-  getRegistrationPage(req, res);
-});
+router.route("/register").get(getRegistrationPage);
 
-router.route("/login").get((req, res) => {
-  console.log("login route accessed");
-  getLoginPage(req, res);
-});
-
+router.route("/login").get(getLoginPage);
 
 router.route("/registered").post(registerUser);
+
 router.route("/loggedin").post(loginUser);
+
+router.route("/logout").get(logout);
 
 module.exports = router;
