2051ffd88b2267a0331b9ec3f410405fc804ada5
jptlearn <jptcode500@gmail.com>
2025-01-22 14:46:10 +0545

    refactor callback logic

--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -1,49 +1,43 @@
-const jwt = require("jsonwebtoken");
 const passport = require("passport");
-const { User } = require("../models");
 
-const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
-
-//* Redirects the user to Google's OAuth 2.0 consent screen
+// Redirects the user to Google's OAuth 2.0 consent screen
 const googleAuthRedirect = passport.authenticate("google", {
   scope: ["profile", "email"],
   prompt: "select_account",
 });
 
+// Handles the Google OAuth callback
+// const googleAuthCallback = (req, res, next) => {
+//   passport.authenticate("google", { failureRedirect: "/auth/user/login" }, (err, user, info) => {
+//     if (err) {
+//       return next(err);
+//     }
 
-// Handles the Google OAuth callback, checks/creates the user, and generates a JWT
+//     if (!user) {
+//       return res.redirect("/auth/user/login");
+//     }
 
-const googleAuthCallback = async (req, res) => {
-  try {
-    // Check if user exists in the database
-    let user = await User.findOne({ where: { email: req.user.email } });
+//     // Successful authentication
+//     req.logIn(user, (err) => {
+//       if (err) {
+//         return next(err);
+//       }
 
-    if (!user) {
-      // If user doesn't exist, create a new record
-      user = await User.create({
-        email: req.user.email,
-        username: req.user.displayName,
-        password: null, // No password for Google-authenticated users
-      });
-    }
+//       // Redirect to the welcome page or any other page
+//       res.redirect("/welcomePage");
+//     });
+//   })(req, res, next);
+// };
 
-    // Generate a JWT token
-    const token = jwt.sign({ id: user.id }, JWT_SECRET, {
-      expiresIn: JWT_EXPIRES_IN,
-    });
-
-    // Set the JWT as a cookie
-    res.cookie("auth", token);
-
-    // Redirect to the welcome page
-    res.redirect("/welcomePage");
-  } catch (error) {
-    console.error("Error during Google OAuth callback:", error);
-
-    // Redirect to the login page on error
-    res.redirect("/auth/user/login");
-  }
+const googleAuthCallback = (req, res, next) => {
+  passport.authenticate("google", { failureRedirect: "/auth/user/login" }, (err, user, info) => {
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/welcomePage");
+    })
+  })(req, res, next);
 };
 
 module.exports = { googleAuthRedirect, googleAuthCallback };
-
