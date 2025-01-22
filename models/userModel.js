a262c26e75af4b0f5dea924dace51477b7d71e7f
jptlearn <jptcode500@gmail.com>
2025-01-22 09:40:57 +0545

    refactor user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,58 +1,76 @@
-const bcrypt = require("bcrypt");
-const sequelize = require("../database/dbConfig.js");
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize")
+const bcrypt = require("bcrypt")
 
-const User = sequelize.define(
-  "User",
-  {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: false,
-      validate: {
-        len: [3, 50],
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: false,
-      validate: {
-        isEmail: true,
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [6, 100]
-      }
-    },
-    confirmPassword: {
-      type: DataTypes.VIRTUAL,
-    },
-  },
-  {
-    timestamps: true,
-    tableName: "users",
-    hooks: {
-      beforeCreate: async (user, options) => {
-        user.password = user.password.trim();
-        user.confirmPassword = user.confirmPassword.trim();
-        // Compare password and confirmPassword before creation
-        if (user.confirmPassword !== user.password) {
-          throw new Error("Passwords do not match");
-        }
-        user.password = await bcrypt.hash(user.password, 10);
+const User = (sequelize) => {
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 50],
+        },
       },
-      beforeUpdate: (user, options) => {
-        // Compare password and confirmPassword before updating
-        if (user.confirmPassword && user.confirmPassword !== user.password) {
-          throw new Error("Passwords do not match");
-        }
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: [6, 100],
+        },
+      },
+      confirmPassword: {
+        type: DataTypes.VIRTUAL,
+        allowNull: true,
+        validate: {
+          isConfirmed(value) {
+            if (this.password && value !== this.password) {
+              throw new Error("Passwords do not match")
+            }
+          },
+        },
+      },
+      googleId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
       },
     },
+    {
+      timestamps: true,
+      tableName: "users",
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            user.password = user.password.trim()
+            user.password = await bcrypt.hash(user.password, 10)
+          }
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed("password") && user.password) {
+            user.password = user.password.trim()
+            user.password = await bcrypt.hash(user.password, 10)
+          }
+        },
+      },
+    },
+  )
+
+  User.prototype.validatePassword = async function (password) {
+    return this.password ? bcrypt.compare(password, this.password) : false
   }
-);
 
-module.exports = User;
+  return User
+}
+
+module.exports = User
+
